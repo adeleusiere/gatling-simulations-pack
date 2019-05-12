be1cb34638afbe516c2ebb6bc24eac5fd3cafd2e
Aurélien Deleusière <aurel@webellian.com>
2019-05-12 20:55:59 +0200

    no message

--- simulations/RampSimulation.scala
+++ simulations/RampSimulation.scala
@@ -1,30 +1,40 @@
-package cfcamp2016
+package io.apinow
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
 import scala.concurrent.duration._
+import scala.util.Random
 
 class RampSimulation extends Simulation {
-  val duration = Duration(System.getProperty("duration").toInt, "seconds")
-  val users = System.getProperty("users").toInt
+   val duration = Duration(System.getProperty("duration").toInt, "seconds")
+   val users = System.getProperty("users").toInt
 
-  val httpConf = http
-   .baseURL("http://" + System.getProperty("base"))
-   .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
-   .acceptEncodingHeader("gzip, deflate")
-   .acceptLanguageHeader("en-US,en;q=0.5")
-   .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:42.0) Gecko/20100101 Firefox/42.0")
+   val httpConf = http
+      .baseUrl("http://" + System.getProperty("base") + "/v1")
+      //.authorizationHeader("Basic c3BvcnRzaG9wOmI4YzhmNmFkLTE3NDgtNDI5OC1hMWMxLWMyY2Q1MzY4NmM2YQ==")
+      .basicAuth("sportshop", "b8c8f6ad-1748-4298-a1c1-c2cd53686c6a")
+      .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+      .acceptEncodingHeader("gzip, deflate")
+      .acceptLanguageHeader("en-US,en;q=0.5")
+      .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36")
 
-  val sim = scenario(s"Ramp (Users=$users - Duration=$duration)")
+   val sim = scenario(s"Ramp (Users=$users - Duration=$duration)")
+      .feed(jsonFile("transactions.json").random)
       .exec(
-         http("Get http://" + System.getProperty("base") + "/cfcamp2016/demo1/")
-            .get("/cfcamp2016/demo1/")
-            .check(
-               status.is(200),
-               substring("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-            )
+         http("GET /transactions/{token}")
+            .get("/transactions/${token}")
+            .check(status.is(200))
+            .check(jsonPath("$..token").is("${token}"))
+            .check(jsonPath("$..client").is("sportshop"))
       )
+      .pause(1 second, 3 seconds)
+      .exec(
+         http("GET /transactions/")
+            .get(session => "/transactions/" + Random.alphanumeric.take(3).mkString)
+            .check(status.is(404))
+      )
+      .pause(1 second, 3 seconds)
 
    setUp(
       sim.inject(
